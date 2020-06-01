ac7135a5ee823361b8b5cc4302377c29c2a8c666
Endre Vegh <endre.vegh@yahoo.com>
2020-06-01 18:17:03 +0200

    set up api

---
+++ packages/api/src/functions/graphql.ts
@@ -0,0 +1,33 @@
+import { ApolloServer, gql } from "@saruni/api";
+
+import { db } from "../db";
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello, Saruni!",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ event, context }) => ({
+    headers: event.headers,
+    functionName: context.functionName,
+    event,
+    context,
+  }),
+});
+
+export const handler = server.createHandler({
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
+});
