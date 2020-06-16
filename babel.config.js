a724035aa4bc9e2fd9f9cdde3283f39e70dedbf3
Endre Vegh <endre.vegh@yahoo.com>
2020-06-16 17:29:26 +0200

    update babel config to handle async

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  presets: ["@babel/preset-typescript", "@babel/preset-env"],
+  presets: [
+    "@babel/preset-typescript",
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: true,
+        },
+        useBuiltIns: "usage",
+        corejs: {
+          version: 3.6,
+          proposals: true,
+        },
+      },
+    ],
+  ],
 };
