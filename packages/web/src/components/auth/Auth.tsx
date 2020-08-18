34d27fddfeaa9f28f260428291ebad9d766590c1
Endre Vegh <endre.vegh@yahoo.com>
2020-08-18 21:37:11 +0200

    add auth

---
+++ packages/web/src/components/auth/Auth.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { AuthContext, useJwt } from "@saruni/auth";
+
+export const Auth: React.FC = (props) => {
+  const { setToken } = useJwt();
+
+  return (
+    <AuthContext.Provider
+      value={{
+        defaultRedirect: "/",
+        isAuthenticated: false,
+        loading: false,
+        login: () => {},
+        logout: () => {},
+        signup: () => {},
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
